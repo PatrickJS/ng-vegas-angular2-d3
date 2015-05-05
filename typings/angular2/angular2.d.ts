eef602c0874a1edb966d23409ea26066c51b5803
gdi2290 <github@gdi2290.com>
2015-05-05 16:38:42 -0700

    chore: typing angular2

---
+++ typings/angular2/angular2.d.ts
@@ -0,0 +1,128 @@
+interface List<T> extends Array<T> {
+}
+interface Type {}
+interface ElementRef {
+  domElement()
+}
+
+declare module "angular2/angular2" {
+  function bootstrap(appComponentType: any): void;
+  function Component({
+    selector,
+    properties,
+    hostListeners,
+    injectables,
+    lifecycle,
+    changeDetection
+    }:{
+      selector:string,
+      properties?:Object,
+      hostListeners?:Object,
+      injectables?:List<any>,
+      lifecycle?:List<any>,
+      changeDetection?:string
+    });
+  function Directive({
+    selector,
+    properties,
+    hostListeners,
+    injectables,
+    lifecycle,
+    changeDetection
+    }:{
+      selector:string,
+      properties?:Object,
+      hostListeners?:Object,
+      injectables?:List<any>,
+      lifecycle?:List<any>,
+      changeDetection?:string
+    });
+
+  function View({
+      templateUrl,
+      template,
+      directives,
+      formatters,
+      source,
+      locale,
+      device
+    }: {
+      templateUrl?: string,
+      template?: string,
+      directives?: List<Type>,
+      formatters?: List<Type>,
+      source?: List<any>,
+      locale?: string,
+      device?: string
+    });
+  function For();
+  function If();
+}
+
+declare module "angular2/directives" {
+  function For();
+  function If();
+}
+
+
+declare module "angular2/annotations" {
+  var onChange: string;
+  var onDestroy: string;
+  var onAllChangesDone: string;
+  function Attribute(attributeName: string);
+
+  function Component({
+  selector,
+  properties,
+  hostListeners,
+  injectables,
+  lifecycle,
+  changeDetection
+  }:{
+    selector:string,
+    properties?:Object,
+    hostListeners?:Object,
+    injectables?:List<any>,
+    lifecycle?:List<any>,
+    changeDetection?:string
+  });
+
+  function Directive({
+    selector,
+    properties,
+    hostListeners,
+    injectables,
+    lifecycle,
+    changeDetection
+    }:{
+      selector:string,
+      properties?:Object,
+      hostListeners?:Object,
+      injectables?:List<any>,
+      lifecycle?:List<any>,
+      changeDetection?:string
+    });
+
+  function View({
+      templateUrl,
+      template,
+      directives,
+      formatters,
+      source,
+      locale,
+      device
+    }: {
+      templateUrl?: string,
+      template?: string,
+      directives?: List<Type>,
+      formatters?: List<Type>,
+      source?: List<any>,
+      locale?: string,
+      device?: string
+    });
+}
+
+declare module "angular2/src/core/compiler/element_ref" {
+
+  function ElementRef();
+}
