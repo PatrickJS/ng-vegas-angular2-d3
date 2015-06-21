cf1c0395dcc95329031de842781b2d92cdd996ad
gdi2290 <github@gdi2290.com>
2015-06-21 16:21:08 -0700

    chore: include typings

---
+++ typings/_custom/ng2.d.ts
@@ -0,0 +1,4 @@
+declare module "angular2/di" {
+  var Inject: any;
+  function bind(token: any): any;
+}
---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="_custom/ng2.d.ts" />
+/// <reference path="angular2/angular2.d.ts" />
+/// <reference path="d3/d3.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="rx/rx-lite.d.ts" />
+/// <reference path="rx/rx.d.ts" />
